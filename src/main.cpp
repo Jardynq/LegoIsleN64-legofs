242a383e356097cb28793598f1d6ad95be70c48e
Christian Jordan <gcp547@alumni.ku.dk>
2025-04-02 19:13:47 +0200

    Added missing file extension for remaining types

--- src/main.cpp
+++ src/main.cpp
@@ -15,7 +15,8 @@ static std::string destination;
 namespace fs = std::filesystem;
 using namespace si;
 
-enum class Type : unsigned char {
+enum class Type : unsigned char
+{
 	Null = 0,
 	Bitmap = 1,
 	Wave = 2,
@@ -28,12 +29,13 @@ enum class Type : unsigned char {
 	Event = 9,
 };
 
-struct Node {
+struct Node
+{
 	Type type;
 	unsigned short index;
-	const char* name;
-	const char* path;
-	const char* presenter;
+	const char *name;
+	const char *path;
+	const char *presenter;
 	unsigned short start_time;
 	unsigned short duration;
 	unsigned char loops;
@@ -45,33 +47,39 @@ struct Node {
 	std::vector<unsigned short> children;
 };
 
-struct Index {
+struct Index
+{
 	std::vector<Node> nodes;
-	std::unordered_map<const char*, Node*> name_to_node;
-	std::unordered_map<const char*, Node*> path_to_node;
+	std::unordered_map<const char *, Node *> name_to_node;
+	std::unordered_map<const char *, Node *> path_to_node;
 };
 
-const char* to_owned(const std::string& str) {
-	char* result = (char*) malloc(str.length() + 1);
+const char *to_owned(const std::string &str)
+{
+	char *result = (char *)malloc(str.length() + 1);
 	memcpy(result, str.c_str(), str.length());
 	result[str.length()] = 0;
 	return result;
 }
 
-void handle_node(Core* core_parent, Node* parent, Index* tree) {
-	if (core_parent == nullptr) {
+void handle_node(Core *core_parent, Node *parent, Index *tree)
+{
+	if (core_parent == nullptr)
+	{
 		return;
 	}
 
-	for (auto child : core_parent->GetChildren()) {
-		auto object = (Object*) child;
+	for (auto child : core_parent->GetChildren())
+	{
+		auto object = (Object *)child;
 
 		Node node;
 		node.index = object->id();
 		node.path = to_owned(object->filename());
 		node.name = to_owned(object->name());
 		node.presenter = to_owned(object->presenter_);
-		for (auto byte : object->extra_) {
+		for (auto byte : object->extra_)
+		{
 			node.extra.push_back(byte);
 		}
 
@@ -89,15 +97,17 @@ void handle_node(Core* core_parent, Node* parent, Index* tree) {
 		node.up[1] = object->up_.y;
 		node.up[2] = object->up_.z;
 
-		const char* extension = "";
-		switch (object->type()) {
+		const char *extension = "";
+		switch (object->type())
+		{
 		case MxOb::Null:
 			continue;
 		case MxOb::Video:
 		case MxOb::Sound:
 		case MxOb::Bitmap:
 		case MxOb::Object:
-			switch (object->filetype()) {
+			switch (object->filetype())
+			{
 			case MxOb::WAV:
 				node.type = Type::Wave;
 				extension = ".wav";
@@ -110,9 +120,12 @@ void handle_node(Core* core_parent, Node* parent, Index* tree) {
 				node.type = Type::Flic;
 				// Stolen from the qt viewer.
 				// Used to flip the flc upside down.
-				if (object->name().find("_Pho_") != std::string::npos) {
+				if (object->name().find("_Pho_") != std::string::npos)
+				{
 					extension = ".pho";
-				} else {
+				}
+				else
+				{
 					extension = ".flc";
 				}
 				break;
@@ -134,12 +147,15 @@ void handle_node(Core* core_parent, Node* parent, Index* tree) {
 
 		case MxOb::World:
 			node.type = Type::World;
+			extension = ".world";
 			break;
 		case MxOb::Presenter:
 			node.type = Type::Presenter;
+			extension = ".pres";
 			break;
 		case MxOb::Event:
 			node.type = Type::Event;
+			extension = ".event";
 			break;
 
 		case MxOb::TYPE_COUNT:
@@ -150,22 +166,29 @@ void handle_node(Core* core_parent, Node* parent, Index* tree) {
 		fflush(stdout);
 
 		auto object_path = destination + std::to_string(node.index) + extension;
-		if (object->HasChildren()) {
+		if (object->HasChildren())
+		{
 			handle_node(object, &node, tree);
-		} else {
+		}
+		else
+		{
 			bool is_empty = true;
-			for (auto chunk : object->data()) {
-				if (chunk.size() != 0) {
+			for (auto chunk : object->data())
+			{
+				if (chunk.size() != 0)
+				{
 					is_empty = false;
 					break;
 				}
 			}
-			if (!is_empty) {
+			if (!is_empty)
+			{
 				object->ExtractToFile(object_path.c_str());
 			}
 		}
 
-		if (parent != nullptr) {
+		if (parent != nullptr)
+		{
 			parent->children.push_back(node.index);
 		}
 
@@ -173,33 +196,41 @@ void handle_node(Core* core_parent, Node* parent, Index* tree) {
 	}
 }
 
-void handle(Interleaf& weaver, Index* tree, const char* path) {
+void handle(Interleaf &weaver, Index *tree, const char *path)
+{
 	printf("Reading %s\n", path);
-	if (weaver.Read(path) != Interleaf::ERROR_SUCCESS) {
+	if (weaver.Read(path) != Interleaf::ERROR_SUCCESS)
+	{
 		printf("Failed to read %s", path);
 	}
-	handle_node((Core*) &weaver, nullptr, tree);
+	handle_node((Core *)&weaver, nullptr, tree);
 	printf("\n");
 }
 
 // fwrite2 is an fwrite wrapper that swaps endiannes before writing.
 // N64 uses big endian while x86_64 uses little endian.
-#define BYTE(x) ((uint8_t) ((x) & 0xFF))
+#define BYTE(x) ((uint8_t)((x) & 0xFF))
 template <typename T>
-void fwrite2(T* ptr, size_t size, size_t count, FILE* file) {
-	uint8_t* data = (uint8_t*) ptr;
-	if (size == 2) {
-		uint16_t d = *(uint16_t*) ptr;
+void fwrite2(T *ptr, size_t size, size_t count, FILE *file)
+{
+	uint8_t *data = (uint8_t *)ptr;
+	if (size == 2)
+	{
+		uint16_t d = *(uint16_t *)ptr;
 		data[0] = BYTE(d >> 8);
 		data[1] = BYTE(d >> 0);
-	} else if (size == 4) {
-		uint32_t d = *(uint32_t*) ptr;
+	}
+	else if (size == 4)
+	{
+		uint32_t d = *(uint32_t *)ptr;
 		data[0] = BYTE(d >> 24);
 		data[1] = BYTE(d >> 16);
 		data[2] = BYTE(d >> 8);
 		data[3] = BYTE(d >> 0);
-	} else if (size == 8) {
-		uint64_t d = *(uint64_t*) ptr;
+	}
+	else if (size == 8)
+	{
+		uint64_t d = *(uint64_t *)ptr;
 		data[0] = BYTE(d >> 56);
 		data[1] = BYTE(d >> 48);
 		data[2] = BYTE(d >> 40);
@@ -212,7 +243,8 @@ void fwrite2(T* ptr, size_t size, size_t count, FILE* file) {
 	fwrite(data, size, count, file);
 }
 
-void write_node(const Node& node, FILE* file) {
+void write_node(const Node &node, FILE *file)
+{
 	fwrite2(&node.type, sizeof(node.type), 1, file);
 	fwrite2(&node.index, sizeof(node.index), 1, file);
 
@@ -244,40 +276,50 @@ void write_node(const Node& node, FILE* file) {
 
 	size = node.extra.size();
 	fwrite2(&size, sizeof(size), 1, file);
-	for (auto byte : node.extra) {
+	for (auto byte : node.extra)
+	{
 		fwrite2(&byte, sizeof(byte), 1, file);
 	}
 
 	size = node.children.size();
 	fwrite2(&size, sizeof(size), 1, file);
-	for (auto child : node.children) {
+	for (auto child : node.children)
+	{
 		fwrite2(&child, sizeof(child), 1, file);
 	}
 }
 
-void fill_holes(Index& tree) {
+void fill_holes(Index &tree)
+{
 	int max_index = 0;
-	for (unsigned int i = 0; i < tree.nodes.size(); i++) {
-		if (tree.nodes[i].index > max_index) {
+	for (unsigned int i = 0; i < tree.nodes.size(); i++)
+	{
+		if (tree.nodes[i].index > max_index)
+		{
 			max_index = tree.nodes[i].index;
 		}
 	}
 
 	auto holes = std::vector<unsigned int>();
-	for (int i = 0; i < max_index; i++) {
+	for (int i = 0; i < max_index; i++)
+	{
 		bool found = false;
-		for (unsigned int j = 0; j < tree.nodes.size(); j++) {
-			if (tree.nodes[j].index == i) {
+		for (unsigned int j = 0; j < tree.nodes.size(); j++)
+		{
+			if (tree.nodes[j].index == i)
+			{
 				found = true;
 				break;
 			}
 		}
-		if (!found) {
+		if (!found)
+		{
 			holes.push_back(i);
 		}
 	}
 
-	for (auto index : holes) {
+	for (auto index : holes)
+	{
 		Node node;
 		node.type = Type::Null;
 		node.index = index;
@@ -290,49 +332,60 @@ void fill_holes(Index& tree) {
 	}
 }
 
-void write_index(Index& tree) {
-	FILE* file = fopen((destination + "index").c_str(), "wb");
-	if (file == nullptr) {
+void write_index(Index &tree)
+{
+	FILE *file = fopen((destination + "index").c_str(), "wb");
+	if (file == nullptr)
+	{
 		fprintf(stderr, "Failed to open index file for writing\n");
 		return;
 	}
 
 	unsigned short size = tree.nodes.size();
 	fwrite2(&size, sizeof(size), 1, file);
-	for (auto node : tree.nodes) {
+	for (auto node : tree.nodes)
+	{
 		write_node(node, file);
 	}
 
 	fclose(file);
 }
 
-int main(int argc, char* argv[]) {
-	if (argc != 3) {
+int main(int argc, char *argv[])
+{
+	if (argc != 3)
+	{
 		printf("Usage: sidump <destination> <interleaf file>\n");
 		return 0;
 	}
 
-	const char* path = argv[2];
-	if (!fs::exists(path)) {
+	const char *path = argv[2];
+	if (!fs::exists(path))
+	{
 		fprintf(stderr, "Path does not exist\n");
 		return -1;
 	}
 
 	destination = std::string(argv[1]);
-	if (destination.back() != '/') {
+	if (destination.back() != '/')
+	{
 		destination += '/';
 	}
 	fs::create_directory(destination);
-	if (!fs::is_directory(destination)) {
+	if (!fs::is_directory(destination))
+	{
 		fprintf(stderr, "Destination is not a directory\n");
 		return -1;
 	}
 
 	Index tree;
 	Interleaf weaver;
-	if (!fs::is_directory(path)) {
+	if (!fs::is_directory(path))
+	{
 		handle(weaver, &tree, path);
-	} else {
+	}
+	else
+	{
 		fprintf(stderr, "Path is a directory\n");
 		return -1;
 	}
@@ -343,8 +396,8 @@ int main(int argc, char* argv[]) {
 	std::sort(
 		tree.nodes.begin(),
 		tree.nodes.end(),
-		[](const Node& a, const Node& b) { return a.index < b.index; }
-	);
+		[](const Node &a, const Node &b)
+		{ return a.index < b.index; });
 	write_index(tree);
 
 	return 0;
